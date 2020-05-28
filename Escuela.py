876f398f4ef397b511f78297cbe2f1d33c8f6a78
Barlow22 <hartman.lucasj@gmail.com>
2020-05-28 19:16:52 -0300

    segunda parte del programa

--- Escuela.py
+++ Escuela.py
@@ -2,26 +2,35 @@
 
 class Alumnos():
     def __init__(self, nombre):
-        self.nombre = nombre
-        self.nota_mate = 0
-        self.nota_lengua = 0
-        self.nota_geo = 0
+        self.nombre_alumno = nombre
+        self.notas = {'matematica' : 0, 'lengua' : 0, 'geografia' : 0}
         self.promedio = 0
-        
-    def dar_examen(self, desempeño):
-        return desempeño
+        self.rendimiento = ''
+    def dar_examen(self, rendimiento):
+        if rendimiento == 'malo':
+            self.rendimiento = 'malo'
+        if rendimiento == 'bueno':
+            self.rendimiento = 'bueno'
+        if rendimiento  == 'muy bueno':
+            self.rendimiento = 'muy bueno'
+        if rendimiento == 'excelente':
+            self.rendimiento = 'excelente'
+        print('Tu rendimiento fue ', self.rendimiento)
 
 
 class Profesores(Alumnos):
-    def __init__(self, nombre, materia):
-        self.nombre = nombre
+    def __init__(self, nombre, materia, Alumnos):
+        self.nombre_profesor = nombre
         self.materia = materia
-        Alumnos.__init__(self, nombre)
+        Alumnos.__init__(self)
 
-    def tomar_examen(self, desempeño):
-        if desempeño >= 0 and desempeño <20:
-            self.nota
+    def tomar_examen(self):
+        if self.rendimiento == 'muy bueno':
+            self.notas[self.materia] = self.notas[self.materia : 10]
+        print('El alumno ' + self.nombre_alumno  +' se saco un ' + str(self.notas[self.materia]) + ' en ' + self.materia)
 
+alumno_juan = Alumnos('juan')
+alumno_juan.dar_examen('muy bueno')
 
-
-
+prof_pedro = Profesores('Pedro', 'matematica', alumno_juan)
+prof_pedro.tomar_examen()
